19b0987e17ada5e6456b850db65b1410bf6b348c
m2i-formation-cdi <formation@smaloron.eu>
2019-04-30 15:05:44 +0200

    deploy

---
+++ precache-manifest.94a790a8f30d6da2d2b607c33dcf7f43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da7a190209098b05f1a505c0b56d545",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b3336f764ff5081fe41",
+    "url": "/static/css/main.66e38bdf.chunk.css"
+  },
+  {
+    "revision": "3b8ffd6b993c5e1bc2d4",
+    "url": "/static/js/2.bbd0939d.chunk.js"
+  },
+  {
+    "revision": "2b3336f764ff5081fe41",
+    "url": "/static/js/main.3c2225cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
